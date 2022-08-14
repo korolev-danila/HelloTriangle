e1784e202100d107f197595cd8212a0c21b81a7f
Danila <korolevdanila97@gmail.com>
2022-08-14 20:50:24 +0300

    Save progress

---
+++ MetalHelloTriangle/MetalHelloTriangleApp.swift
@@ -0,0 +1,18 @@
+//
+//  MetalHelloTriangleApp.swift
+//  MetalHelloTriangle
+//
+//  Created by Данила on 14.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MetalHelloTriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .frame(width: 800, height: 600)
+        }
+    }
+}
